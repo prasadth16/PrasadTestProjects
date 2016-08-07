3eeadd059a0b0b0cab4a3178c2d2559bd5332639
prasadth16 <dattaprasad.s.thakur@gmail.com>
2016-08-07 16:35:48 -0400

    commit successfull

---
+++ aggrigationProject/src/main/java/thakur/dattaprasad/aggrigationProject/App.java
@@ -0,0 +1,13 @@
+package thakur.dattaprasad.aggrigationProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
